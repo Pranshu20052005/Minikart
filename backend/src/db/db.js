ba0ba66c9cf0b9838bf46dd76ee13fed6e832493
Pranshu20052005 <pranshukansotia154@gmail.com>
2025-07-31 16:17:02 +0530

    mongo

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const connect = ()=>{
-    // mongoose.connect(process.env.MONGODB_URI)
+    mongoose.connect(process.env.MONGODB_URI)
     mongoose.connect("mongodb+srv://magma7094:FDYjFiAy6UKQc0Dh@cluster0.xxuyjjx.mongodb.net/")
     // mongoose.connect("mongodb://localhost:27017/VIPS")
     .then(()=>{
