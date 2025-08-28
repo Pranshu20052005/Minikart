967e21cca764f6a4008b9944eb167fddff1a56a6
Pranshu20052005 <pranshukansotia154@gmail.com>
2025-08-28 16:46:55 +0530

    hi

--- backend/src/models/cart.model.js
+++ backend/src/models/cart.model.js
@@ -3,16 +3,32 @@ const mongoose = require("mongoose");
 const cartSchema = new mongoose.Schema({
   productId: {
     type: mongoose.Schema.Types.ObjectId,
-    required: true,
-    ref: "product", 
+    required: [true, 'Product ID is required'],
+    ref: 'Product' // Ensure this matches your product model name
   },
   quantity: {
     type: Number,
-    required: true,
+    required: [true, 'Quantity is required'],
     default: 1,
-    min: 1
+    min: [1, 'Quantity cannot be less than 1'],
+    validate: {
+      validator: Number.isInteger,
+      message: 'Quantity must be an integer'
+    }
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'User ID is required']
   }
+}, {
+  timestamps: true, // Adds createdAt and updatedAt fields
+  toJSON: { virtuals: true },
+  toObject: { virtuals: true }
 });
+
+// Compound index to ensure a user can't add the same product to cart multiple times
+cartSchema.index({ userId: 1, productId: 1 }, { unique: true });
 
 const cartModel = mongoose.model("Cart", cartSchema);
 
